cee450ca98bc4be0a1c549dc09231c7530584f61
JJetmar <JJetmar@users.noreply.github.com>
2025-01-08 10:29:03 +0100

    Improve repository to human readable form

--- tempermonkey-github-branch-suggestion.user.js
+++ tempermonkey-github-branch-suggestion.user.js
@@ -22,7 +22,7 @@
             const issueNumber = $('.js-issue-title + span').eq(0).text().replace(/[^\d]/g, '');
             const projectPath = $('nav[aria-label="Page context"]').text().replace(/\s+/mg, '');
             const [organization, repository] = projectPath.split('/');
-
+            const humanReadableRepository = repository.substring(0, 1).toUpperCase().concat(repository.substring(1).replace(/-+/g, ' '))
             const branchName = `fix/${issueNumber}-${formattedIssueName}`
 
             if (branchName && lastBranchName !== branchName) {
@@ -31,7 +31,7 @@
                 $('.gh-header-meta').parent().append(`<br>Commit message suggestion: <input type="text" value="fix($actorName): #${issueNumber} - ${issueTitle}" readonly id="branch-name-suggestion" size="100">`);
                 $('.gh-header-meta').parent().append(`<br>DailyBot Report:<br>`
                                                      + `<div><code>`
-                                                     + `<strong>${repository}</strong><br>`
+                                                     + `<strong>${humanReadableRepository}</strong><br>`
                                                      + `• <a href="${location.href}">#${issueNumber} - ${issueTitle}</a>`
                                                      + `</code></div>`);
                 lastBranchName = branchName;
