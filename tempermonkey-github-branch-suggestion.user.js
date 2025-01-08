3aee58475b9d0626c73c33c1b530f6afef6c3786
JJetmar <JJetmar@users.noreply.github.com>
2025-01-08 10:32:09 +0100

    Increase version

--- tempermonkey-github-branch-suggestion.user.js
+++ tempermonkey-github-branch-suggestion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Issue copy-paste
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://github.com/*
