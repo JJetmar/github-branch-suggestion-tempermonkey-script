541c32ab40ec96530a9da6f5d6f8dd4a8a927ffe
JJetmar <JJetmar@users.noreply.github.com>
2025-01-22 10:54:29 +0100

    Update tempermonkey-github-branch-suggestion.user.js

--- tempermonkey-github-branch-suggestion.user.js
+++ tempermonkey-github-branch-suggestion.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Github Issue copy-paste
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
-// @author       You
+// @author       https://github.com/JJetmar/
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @require      https://code.jquery.com/jquery-3.6.3.min.js
@@ -17,19 +17,19 @@
 
     setInterval(() => {
         if (/\/issues\//.test(location.href)) { // Evaluate only for issues (not PRs for example)
-            const issueTitle = $('.js-issue-title').eq(0).text();
+            const issueTitle = $('[data-testid="issue-title"]').eq(0).text();
             const formattedIssueName = issueTitle.replace(/[^a-zA-Z\d]+/g, '-').replace(/-/g, '-').replace(/^-+|-+$/g, '').toLocaleLowerCase()
-            const issueNumber = $('.js-issue-title + span').eq(0).text().replace(/[^\d]/g, '');
+            const issueNumber = $('[data-testid="issue-title"] + span').eq(0).text().replace(/[^\d]/g, '');
             const projectPath = $('nav[aria-label="Page context"]').text().replace(/\s+/mg, '');
             const [organization, repository] = projectPath.split('/');
             const humanReadableRepository = repository.substring(0, 1).toUpperCase().concat(repository.substring(1).replace(/-+/g, ' '))
             const branchName = `fix/${issueNumber}-${formattedIssueName}`
 
             if (branchName && lastBranchName !== branchName) {
-                $('#branch-name-suggestion').remove();
-                $('.gh-header-meta').parent().append(`Branch name suggestion: <input type="text" value="${branchName}" readonly id="branch-name-suggestion" size="100">`);
-                $('.gh-header-meta').parent().append(`<br>Commit message suggestion: <input type="text" value="fix($actorName): #${issueNumber} - ${issueTitle}" readonly id="branch-name-suggestion" size="100">`);
-                $('.gh-header-meta').parent().append(`<br>DailyBot Report:<br>`
+                //$('#branch-name-suggestion').remove();
+                $('[data-testid="issue-header"]').parent().append(`Branch name suggestion: <input type="text" value="${branchName}" readonly id="branch-name-suggestion" size="100">`);
+                $('[data-testid="issue-header"]').parent().append(`<br>Commit message suggestion: <input type="text" value="fix($actorName): #${issueNumber} - ${issueTitle}" readonly id="branch-name-suggestion" size="100">`);
+                $('[data-testid="issue-header"]').parent().append(`<br>DailyBot Report:<br>`
                                                      + `<div><code>`
                                                      + `<strong>${humanReadableRepository}:</strong><br>`
                                                      + `• <a href="${location.href}">#${issueNumber} - ${issueTitle}</a>`
