87554b27eb8ad958bfcb7788c54559633f784019
JJetmar <josef.jetmar@apify.com>
2025-06-20 13:39:01 +0200

    Fixed based on new GitHub design

--- tempermonkey-github-branch-suggestion.user.js
+++ tempermonkey-github-branch-suggestion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Issue copy-paste
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       https://github.com/JJetmar/
 // @match        https://github.com/*
@@ -17,23 +17,26 @@
 
     setInterval(() => {
         if (/\/issues\//.test(location.href)) { // Evaluate only for issues (not PRs for example)
-            const issueTitle = $('[data-testid="issue-title"]').eq(0).text();
+            const issueTitleElement = $('[data-component="PH_Title"]').eq(0);
+            const issueTitle = issueTitleElement.find('*').first().text();
             const formattedIssueName = issueTitle.replace(/[^a-zA-Z\d]+/g, '-').replace(/-/g, '-').replace(/^-+|-+$/g, '').toLocaleLowerCase()
-            const issueNumber = $('[data-testid="issue-title"] + span').eq(0).text().replace(/[^\d]/g, '');
-            const projectPath = $('nav[aria-label="Page context"]').text().replace(/\s+/mg, '');
-            const [organization, repository] = projectPath.split('/');
+            const issueNumber = issueTitleElement.find('*').last().text().replace(/[^\d]/g, '');
+            const breadCrumElements = $('[data-target="context-region-crumb.linkElement"]');
+            const organization = breadCrumElements.eq(0).text().trim();
+            const repository = breadCrumElements.eq(1).text().trim();
             const humanReadableRepository = repository.substring(0, 1).toUpperCase().concat(repository.substring(1).replace(/-+/g, ' '))
             const branchName = `fix/${issueNumber}-${formattedIssueName}`
 
             if (branchName && lastBranchName !== branchName) {
                 //$('#branch-name-suggestion').remove();
-                $('[data-testid="issue-header"]').parent().append(`Branch name suggestion: <input type="text" value="${branchName}" readonly id="branch-name-suggestion" size="100">`);
-                $('[data-testid="issue-header"]').parent().append(`<br>Commit message suggestion: <input type="text" value="fix($actorName): #${issueNumber} - ${issueTitle}" readonly id="branch-name-suggestion" size="100">`);
-                $('[data-testid="issue-header"]').parent().append(`<br>DailyBot Report:<br>`
-                                                     + `<div><code>`
-                                                     + `<strong>${humanReadableRepository}:</strong><br>`
-                                                     + `• <a href="${location.href}">#${issueNumber} - ${issueTitle}</a>`
-                                                     + `</code></div>`);
+                const titleParentElement = issueTitleElement.parent().parent().parent();
+                titleParentElement.append(`Branch name suggestion: <input type="text" value="${branchName}" readonly id="branch-name-suggestion" size="100">`);
+                titleParentElement.append(`<br>Commit message suggestion: <input type="text" value="fix($actorName): #${issueNumber} - ${issueTitle}" readonly id="branch-name-suggestion" size="100">`);
+                titleParentElement.append(`<br>DailyBot Report:<br>`
+                    + `<div><code>`
+                    + `<strong>${humanReadableRepository}:</strong><br>`
+                    + `• <a href="${location.href}">#${issueNumber} - ${issueTitle}</a>`
+                    + `</code></div>`);
                 lastBranchName = branchName;
             }
         }
